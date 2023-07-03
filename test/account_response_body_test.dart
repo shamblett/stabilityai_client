e793237604096f32d9a4175788a88ec64dfa3379
shamblett <steve.hamblett@linux.com>
2023-07-03 15:05:46 +0100

    Account response body test complete

--- test/account_response_body_test.dart
+++ test/account_response_body_test.dart
@@ -13,31 +13,52 @@ import 'package:test/test.dart';
 
 // tests for AccountResponseBody
 void main() {
-  // final instance = AccountResponseBody();
-
   group('test AccountResponseBody', () {
+    final org1 = OrganizationMembership(
+        id: '1', isDefault: true, name: 'org1', role: 'admin');
+    final org2 = OrganizationMembership(
+        id: '2', isDefault: false, name: 'org2', role: 'user');
+    final body = <String, dynamic>{
+      'email': 'me@here.com',
+      'id': 'me',
+      'organizations': <dynamic>[org1.toJson(), org2.toJson()],
+      'profile_picture': 'darticulate.com/profile_picture'
+    };
+
+    test('Not a map', () {
+      final instance = AccountResponseBody.fromJson('');
+      expect(instance, isNull);
+    });
+
+    test('Required keys', () {
+      expect(() => AccountResponseBody.fromJson(<String, dynamic>{}),
+          throwsA(isA<AssertionError>()));
+    });
+
+    final instance = AccountResponseBody.fromJson(body);
+
     // The user's email
     // String email
     test('to test the property `email`', () async {
-      // TODO
+      expect(instance?.email, 'me@here.com');
     });
 
     // The user's ID
     // String id
     test('to test the property `id`', () async {
-      // TODO
+      expect(instance?.id, 'me');
     });
 
     // The user's organizations
     // List<OrganizationMembership> organizations (default value: const [])
     test('to test the property `organizations`', () async {
-      // TODO
+      expect(instance?.organizations, [org1, org2]);
     });
 
     // The user's profile picture
     // String profilePicture
     test('to test the property `profilePicture`', () async {
-      // TODO
+      expect(instance?.profilePicture, 'darticulate.com/profile_picture');
     });
   });
 }
