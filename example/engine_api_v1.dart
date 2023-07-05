062da955217896d7edf30ca6c7b4816a48f4c546
Steve Hamblett <steve.hamblett@linux.com>
2023-07-05 10:37:03 +0100

    Engines example

--- example/engine_api_v1.dart
+++ example/engine_api_v1.dart
@@ -10,10 +10,10 @@ import 'package:stabilityai_client/stabilityai_client.dart';
 /// An example of how to use the Stability API V1 to list
 /// all engines available to your organization/user.
 ///
-void main() {
+Future<void> main() async {
   // Create an API client with API key authentication
   final client = ApiClient(
-      authentication: ApiKeyAuth('', '')..apiKey = 'STABILITY_API_KEY');
+      authentication: ApiKeyAuth('header', 'Authorization')..apiKey = '');
 
   // Get an instance of the V1 engines API using our client
   final apiInstance = V1EnginesApi(client);
@@ -22,7 +22,7 @@ void main() {
 
   // Allows for requests to be scoped to an organization other than the user's
   // default.  If not provided, the user's default organization will be used.
-  final organization = 'org-123456';
+  //final organization = ''; //'''org-123456';
   // Used to identify the source of requests, such as the client application or sub-organization.
   // Optional, but recommended for organizational clarity.
   final stabilityClientID = 'my-great-plugin';
@@ -31,8 +31,8 @@ void main() {
   final stabilityClientVersion = '1.0.0';
 
   try {
-    final result = apiInstance.listEngines(
-        organization: organization,
+    final result = await apiInstance.listEngines(
+        //organization: organization,
         stabilityClientID: stabilityClientID,
         stabilityClientVersion: stabilityClientVersion);
     print(result);
