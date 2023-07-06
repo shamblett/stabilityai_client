cc780b9f2b664969d20bd2c5418ece4d74b9e88a
Steve Hamblett <steve.hamblett@linux.com>
2023-07-06 21:07:27 +0100

    Generation example

--- example/generation_api_v1.dart
+++ example/generation_api_v1.dart
@@ -7,12 +7,18 @@
 
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:stabilityai_client/stabilityai_client.dart';
 
 /// An example of how to use the Stability API V1 to generate a new image from a text prompt
 
 Future<void> main() async {
+
+  final imagesGenerated = <Uint8List>[];
+  final tmpDir = Directory.systemTemp.path;
+  const fileNamePrefix = 'yourimage';
+
   // Create an API client with API key authentication
   final client = StabilityaiClient.getApiKeyAuthClient('YOUR-STABILITY-API-KEY');
 
@@ -41,18 +47,19 @@ Future<void> main() async {
         stabilityClientID: stabilityClientID,
         stabilityClientVersion: stabilityClientVersion);
     print('');
-    final fileName = 'yourimage';
     var count = 1;
     if (result!.isNotEmpty) {
       print('Image details --> ');
       print('');
       for (final image in result) {
+        final fileName = '$tmpDir$fileNamePrefix-$count.png';
         print('Finish Reason : ${image.finishReason}');
         print('Seed: ${image.seed}');
         final png = base64Decode(image.base64!);
-        final file = File('$fileName-$count.png');
+        imagesGenerated.add(png);
+        final file = File(fileName);
         file.writeAsBytesSync(png);
-        print('Your image is in the file ../$fileName-$count.png');
+        print('Your image is in the file $fileName');
         count++;
       }
     } else {
